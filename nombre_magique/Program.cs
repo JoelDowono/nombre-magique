814c2f8f16ab244419b70459ee288355f370bf7e
joel <joeldowono1@gmail.com>
2023-01-13 20:00:57 +0100

    fonction qui permet de demander un nombre

---
+++ nombre_magique/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static int DemanderNombre()
+        {
+            int nombre_num = 0;
+            while (nombre_num == 0)
+            {
+                Console.Write("entrez un nombre: ");
+                string nombre_str = Console.ReadLine();
+
+                try
+                {
+                    nombre_num = int.Parse(nombre_str);
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur: Ce nombre n'est pas valide.");
+                }
+            }
+            return nombre_num;
+        }
+
+        static void Main(string[] args)
+        {
+            int nombre = DemanderNombre();
+            Console.WriteLine("votre nombre est: " + nombre);
+        }
+    }
+}
